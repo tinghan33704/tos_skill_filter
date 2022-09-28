4e8573ed39dba8f7c43c48bd577c5e640de85975
tinghan33704 <tinghan33704@gmail.com>
2022-09-29 00:48:44 +0800

    add saitama easter egg

--- js/index.js
+++ js/index.js
@@ -496,6 +496,21 @@ function startFilter()
         return tag_html;
     });
 	
+	// Saitama easter egg :)
+	function showGlassBreak(x, y) {
+		const scale = 1 + Math.random() * 0.8
+		const angle = Math.random() * 360
+		$('<img/>',{
+			src: '../tos_tool_data/img/other/glass_break.png',
+			style: `position: absolute; left: ${x - 100}px; top: ${y - 100}px; pointer-events: none; user-select: none; opacity: 0.8; transform: rotate(${angle}deg) scale(${scale});`,
+			unselectable: 'on'
+		}).appendTo('body');
+	}
+	
+	$('.monster_img[src$="/10294.png"]').click(function(e){
+		showGlassBreak(event.pageX, event.pageY)
+	});
+	
     jumpTo("result_title");
 }
 
